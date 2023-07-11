e9783bb66b962672510303a6f2e398a262a5ba4a
Pedro Duca <testandoduca@gmail.com>
2023-07-11 09:22:23 -0300

    Initial Commit

---
+++ Concentration App/Card.swift
@@ -0,0 +1,26 @@
+//
+//  Card.swift
+//  Concentration App
+//
+//  Created by Pedro Henrique Lopes Duca on 11/07/23.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init(){
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
